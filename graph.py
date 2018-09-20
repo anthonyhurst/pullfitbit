5ab430bb1b73c5de6d869b8e6c0edd55fc804a2d
Anthony Hurst <anthony.hurst@gmail.com>
2018-09-20 01:34:23 +0000

    Add graph for sleep.  Both graphs need a ton of work, just getting some starting examples.

--- graph.py
+++ graph.py
@@ -11,6 +11,34 @@ def load_heart_data(filename="heart.json"):
     fig = ax.get_figure()
     fig.savefig("heart.pdf")
 
+def load_sleep_data(filename="sleep.json"):
+    fh = open(filename, "r")
+    content = fh.read()
+    fh.close()
+    obj = json.loads(content.strip())
+    dfs = []
+    i = 0
+    for session in obj["sleep"]:
+        df_tmp = pandas.DataFrame.from_records(session["levels"]["data"])
+        df_tmp["Session"] = i
+        dfs.append(df_tmp)
+        i+=1
+    df = pandas.concat(dfs)
+    colors = {
+        "wake": "red",
+        "rem": "lightblue",
+        "deep": "darkblue",
+        "light": "green",
+    }
+    df["color"] = df["level"].apply(lambda x: colors[x])
+    df["dateTime"] = pandas.to_datetime(df['dateTime'])
+    ax = df.plot(kind="bar", x="dateTime", y="seconds", color=df["color"], figsize=(50, 30))
+    fig = ax.get_figure()
+    fig.savefig("sleep.pdf")
+
+
+
 
 
 load_heart_data()
+load_sleep_data()
